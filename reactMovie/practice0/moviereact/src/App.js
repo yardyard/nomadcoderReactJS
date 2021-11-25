fe2e6c51debe397a4f1d8f2a9b4821168485fa51
yardyard <84224418+yardyard@users.noreply.github.com>
2021-11-25 19:32:54 +0900

    6 2 Coin Tracker

--- reactMovie/practice0/moviereact/src/App.js
+++ reactMovie/practice0/moviereact/src/App.js
@@ -3,39 +3,37 @@ import { useEffect, useState } from "react";
 
 function App() {
   // --------- state ---------
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins , setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    // api로부터 response인자를 통해 데이터를 받고 response에서 json을 추출한 값을 return해준다.
+    .then((response) => response.json())
+    // 그다음 json을 setCoins(modify)에 넣어주고, setLoading에 false값을 return한다.
+    .then((json)=> {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);
   
   // --------- function ---------
-  const onChange=(event) => {
-    setToDo(event.target.value); };
-  
-    const onSubmit=(event) => {
-    // form의 submit 이벤트시 새로고침 되는 것을 막아줌.
-    event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
-    // ...array의 의미는 기존 array에 index들을 가져온다는 의미이다.
-    // 아래 코드는 toDo를 기존 currentArray에 새로운 인덱스로 넣어서 새로운 array를 만든다는 뜻이다.
-    setToDos(currentArray => [toDo, ...currentArray]);
-    // 입력을 제출하였으면 다시 input창을 "" 빈 값을 넣어줌.
-    setToDo("");
-  };
+
+ 
   // --------- rendering ---------
   return (
     <div>
-      <h1>My To-Dos ({toDos.length})</h1>
-      <form>
-        <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..." />
-        <button onClick={onSubmit}>Add to Do</button>
-      </form>
-      <hr />
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {toDos.map((toDo, index)=> <li key={index}>{toDo}</li>)}
+        {/* coin을 map으로 나눠서 coin 변수로 각 코인들을 리턴해준다. */}
+        {coins.map((coin) => 
+        <li>
+          {/* 아래의 coin은 coins state의 array를 map으로 나눈 각각의 coin이다. */}
+          {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+        </li>
+        )}
       </ul>
     </div>
   );
-}
-
+};
 export default App;
